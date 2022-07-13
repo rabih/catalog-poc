8d4c0678e5c5af23f301b6fc758b41dcaabbbfde
Rabih Salamey <rabih.salamey@convictional.com>
2022-07-13 16:47:43 -0400

    stub out header/footer implementation

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,23 +1,13 @@
-import logo from 'images/logo.svg';
+import Footer from 'containers/Footer/Footer';
+import Header from 'containers/Header/Header';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header></Header>
+      <h1>Insert mosiac list</h1>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/containers/Header/Header.jsx
@@ -0,0 +1,5 @@
+export const Header = () => {
+    return <div></div>
+};
+
+export default Header;
