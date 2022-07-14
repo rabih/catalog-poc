87be965baf02b174f4a200fe86df688c0aece842
Rabih Salamey <rabih.salamey@convictional.com>
2022-07-14 13:54:12 -0400

    fetch company id from url parameter

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,14 +1,18 @@
 import Footer from 'containers/Footer/Footer';
 import Header from 'containers/Header/Header';
 import Main from 'containers/Main/Main';
+
 import './App.css';
-import testData from '../../testData';
 
 function App() {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  const companyId = urlParams.get("companyId");
+
   return (
     <div className="App">
       <Header></Header>
-      <Main data={testData}></Main>
+      <Main companyId={companyId}></Main>
       <Footer></Footer>
     </div>
   );
