21ba2561144eaee8674b99a63fccb640bf8c73c2
Alex Beeson <alex@convictional.com>
2022-07-14 15:59:27 -0400

    completed product cards and refactored button component props

--- src/containers/Footer/Footer.jsx
+++ src/containers/Footer/Footer.jsx
@@ -2,12 +2,19 @@ import Button from "containers/Button/Button";
 import "./Footer.css"
 
 export const Footer = () => {
+
+    // Send the button click to Convictional's login page
+    const ctaAction = () => {
+        window.open('app.convictional.com/login', '_blank').focus();
+    }
+
+
     return (
     <div className="Footer">
         <div className="Footer__powered">
             <p>Powered by Convictional</p>
         </div>
-        <Button class="Footer__cta" text="Trade with us" />
+        <Button action={ctaAction} class="Footer__cta" text="Trade with us" />
     </div>
     )
 };
